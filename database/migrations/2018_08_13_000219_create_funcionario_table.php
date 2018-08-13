18251fd3615153a280766e6a71401e0b11805d9d
Tiago Silva Costa <tiagotsc@oi.com.br>
2018-08-12 21:10:01 -0300

    Alterações

---
+++ database/migrations/2018_08_13_000219_create_funcionario_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFuncionarioTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('funcionario', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('matricula')->unique();
+            $table->string('nome');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('funcionario');
+    }
+}
